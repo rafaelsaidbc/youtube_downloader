51f5906505d137bb1c2a61063a14b5d2faee5332
Rafael Said <rafasaid@gmail.com>
2019-11-01 09:51:03 -0300

    Add download audio/mp3 option

---
+++ convert.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/playlist?list=PLeI92SVnXutU6yMMDnQ746mxrF4yNCIkp'])
